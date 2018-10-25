d9b8f80aa5f8922b6f62646d54dd00a0023620be
goongoguma <wogus7an@gmail.com>
2018-10-25 10:50:13 +0900

    Baseball-Game

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hello World!');
-});
+const form = document.querySelector('.number-list');
+let input1 = document.querySelector('.input1');
+let input2 = document.querySelector('.input2');
+let input3 = document.querySelector('.input3');
+
+form.addEventListener('submit', e => {
+  const numberLi = document.createElement('li');
+  let inputAll = input1.value + input2.value + input3.value;
+  numberLi.textContent = inputAll;
+  form.appendChild(numberLi);
+  e.preventDefault();
+  e.target.reset();
+})

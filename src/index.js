1a3248a25e78051d7630ac3aee35a44772a55249
goongoguma <wogus7an@gmail.com>
2018-10-26 11:57:38 +0900

    Baseball Game

--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
-const form = document.querySelector('.number-list');
-const ulEl = document.querySelector('.input-list');
-let input1 = document.querySelector('.input1');
-let input2 = document.querySelector('.input2');
-let input3 = document.querySelector('.input3');
+const form = document.querySelector(".number-list");
+const ulEl = document.querySelector(".input-list");
+let input1 = document.querySelector(".input1");
+let input2 = document.querySelector(".input2");
+let input3 = document.querySelector(".input3");
 
 let order = 1;
 
-const randomNum  = () => {
+const randomNum = () => {
   const numberArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   // 랜덤으로 세자리 숫자 뽑기
-  const one = numberArr.splice(Math.floor(Math.random() * numberArr.length),1,0)
-  const two = numberArr.splice(Math.floor(Math.random() * numberArr.length),1,0)
-  const three = numberArr.splice(Math.floor(Math.random() * numberArr.length),1,0)
-  return one+two+three
-}
-randomNum()
+  const one = numberArr.splice(
+    Math.floor(Math.random() * numberArr.length),
+    1,
+    0
+  );
+  const two = numberArr.splice(
+    Math.floor(Math.random() * numberArr.length),
+    1,
+    0
+  );
+  const three = numberArr.splice(
+    Math.floor(Math.random() * numberArr.length),
+    1,
+    0
+  );
+  return one + two + three;
+};
+randomNum();
 
 // 값을 고정시키기 위해 변수 사용
-const ranNoVa = randomNum()
+const ranNoVa = randomNum();
 
 console.log(ranNoVa);
 
-form.addEventListener('submit', e => {
+form.addEventListener("submit", e => {
   // 예전 코드
   // const numberLi = document.createElement('li')
   // numberLi.classList.add('order')
@@ -29,17 +41,16 @@ form.addEventListener('submit', e => {
   // numberLi.textContent = inputAll;
   // form.appendChild(numberLi);
 
-  const liEl = document.createElement('li');
-  const inning = document.createElement('div');
-  const numberSpan1 = document.createElement('span');
-  const numberSpan2 = document.createElement('span');
-  const numberSpan3 = document.createElement('span');
-  const EmptyDiv = document.createElement('div');
-
+  const liEl = document.createElement("li");
+  const inning = document.createElement("div");
+  const numberSpan1 = document.createElement("span");
+  const numberSpan2 = document.createElement("span");
+  const numberSpan3 = document.createElement("span");
+  const EmptyDiv = document.createElement("div");
 
   // 클래스 및 텍스트 지정
-  inning.classList.add('inningDisplay');
-  EmptyDiv.classList.add('empty');
+  inning.classList.add("inningDisplay");
+  EmptyDiv.classList.add("empty");
   numberSpan1.textContent = input1.value;
   numberSpan2.textContent = input2.value;
   numberSpan3.textContent = input3.value;
@@ -49,7 +60,7 @@ form.addEventListener('submit', e => {
   const li2 = liEl.appendChild(numberSpan2);
   const li3 = liEl.appendChild(numberSpan3);
   const emptyEl = liEl.appendChild(EmptyDiv);
-  const inningEl = liEl.insertBefore(inning,numberSpan1);
+  const inningEl = liEl.insertBefore(inning, numberSpan1);
 
   // 위의 li를 ul에 붙이기
   ulEl.appendChild(li1);
@@ -58,29 +69,40 @@ form.addEventListener('submit', e => {
   ulEl.appendChild(emptyEl);
   ulEl.insertBefore(inningEl, li1);
 
-  // 이렇게 하면 문자열 숫자조합이 나온다.
-  // console.log(numberSpan1.textContent + numberSpan2.textContent + numberSpan3.textContent);
+  // 스트라이크, 볼 판별
+  function game(inputNum) {
+    let strike = 0;
+    let ball = 0;
+    // 사용자 입력 번호 조합
+    inputNum =
+      numberSpan1.textContent +
+      numberSpan2.textContent +
+      numberSpan3.textContent;
 
+    if(inputNum.split("").length < 3) {
+      alert("빈칸이 없도록 입력해주세요.")
+    } else if (inputNum[0] == inputNum[1] && inputNum[1] == inputNum[2]) {
+      alert("중복된 숫자가 없도록 입력해주세요.");
+    }
+
+    // 볼 판별 -
+    for (let i = 0; i < ranNoVa.length; i++) {
+      if (ranNoVa[i] === inputNum[i]) {
+        strike++;
+      } else if (ranNoVa.includes(inputNum[i])) {
+        ball++;
+      }
+    }
+
+    console.log(`${ball}B, ${strike}S`);
+
+    if (strike === 3) {
+      console.log(`${inputNum} 정답`);
+    } else if (strike === 0 && ball === 0) {
+      console.log("OUT");
+    }
+  }
   e.preventDefault();
   e.target.reset();
-
-
-  // // 랜덤숫자와 입력숫자 비교
-  // if(inputAll === ranNoVa) {
-  //   console.log('hello')
-  // } else {
-  //   console.log('bye')
-  // }
-
-  // if(input1.length === 0) {
-  //   console.log('hello')
-  // }
-
-
-})
-
-
-
-
-
-
+  game();
+});
